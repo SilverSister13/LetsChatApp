2c07b662b6016f979ce3eb908fec1bee7a61ee64
SilverSister13 <82624905+SilverSister13@users.noreply.github.com>
2021-07-14 13:05:58 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCRTILRPEwfp0Qt0f1ebWAd5AIV0cV_HRU",
+    authDomain: "kwitter-practice-baabd.firebaseapp.com",
+    databaseURL: "https://kwitter-practice-baabd-default-rtdb.firebaseio.com",
+    projectId: "kwitter-practice-baabd",
+    storageBucket: "kwitter-practice-baabd.appspot.com",
+    messagingSenderId: "758762634815",
+    appId: "1:758762634815:web:efb2d922bb4e618b9f428a",
+    measurementId: "G-ZDFMCG6L5B"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
